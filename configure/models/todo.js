72af3e67fec83efb2c4844ac2a5c80c5808769b2
Loukas Partatilas <108080134+IscaRiot6@users.noreply.github.com>
2023-01-13 23:29:44 +0200

    added schema

---
+++ configure/models/todo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//create schema
+
+const TodoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Todo = mongoose.model('todo', TodoSchema)
