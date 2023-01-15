91a08d1aaf6617765bbe4b2681a039083e41b4ba
Loukas Partatilas <108080134+IscaRiot6@users.noreply.github.com>
2023-01-15 21:30:53 +0200

    added reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import TodoNavbar from './components/TodoNavbar'
 
 function App () {
   return (
     <div className='App'>
-      <h1>To hell with your clan</h1>
+      <TodoNavbar />
     </div>
   )
 }
---
+++ client/src/components/TodoNavbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap'
+
+function TodoNavbar (args) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div>
+      <Navbar color='light' light expand='sm' className='mt-5' {...args}>
+        <NavbarBrand href='/'>TodoList</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='me-auto' navbar>
+            <NavItem>
+              {/* <NavLink href='/components/'>Components</NavLink> */}
+            </NavItem>
+            <NavItem>
+              <NavLink href='https://github.com/IscaRiot6/mvp-todo.git'>
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>What an ugly todo-list!</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default TodoNavbar
